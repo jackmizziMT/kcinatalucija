a8e0a69e3e680ad8c246ba79885b10cd46ab9041
Jack Mizzi <jackmizzi@Jacks-MacBook-Pro.local>
2025-11-09 11:16:29 +0100

    Bump version to 2.0.0

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,3 +1,3 @@
 // Version information for the application
 // This file should be updated manually when the version changes
-export const APP_VERSION = "1.5.1";
+export const APP_VERSION = "2.0.0";
